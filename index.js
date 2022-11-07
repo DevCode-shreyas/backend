5b52acd455febb201ac5395e204f4e31cd59b713
shreyas <shreyasbole4@gmail.com>
2022-11-07 18:24:27 +0530

    my 1st prodcution app

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const format = require("date-format");
+
+const app = express();
+
+const PORT = 4000 || process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.status(200).send("hello js");
+});
+
+app.get("/api/v1/:token", (req, res) => {
+  console.log(req.params.token);
+  res.status(200).json({ param: req.params.token });
+});
+
+app.get("/api/v1/instagram", (req, res) => {
+  const instaSocial = {
+    userName: "shreyas",
+    followers: 44,
+    follows: 44,
+    date: format.asString("dd[MM] - hh:mm:ss", new Date()),
+  };
+  res.status(200).json(instaSocial);
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running at ${PORT}`);
+});
